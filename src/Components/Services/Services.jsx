20bf066b3ae80cacdeb9889860e9ca6ef5894113
Yash Rawat <yashrawat1905@gmail.com>
2024-09-04 10:33:47 +0530

    Added Sevices section

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,24 +1,60 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import './Services.css'
+import React, {useState} from 'react';
+import './Services.css';
+import Form from '../Form/Form';
 
 const Services = () => {
+    const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const openForm = () => setIsFormOpen(true);
+  const closeForm = () => setIsFormOpen(false);
   return (
     <div className='services'>
+      <p className="services-heading">
+        <strong>Logistics Solutions</strong> created for your business needs
+      </p>
+      <div className='services-container'>
         <div className='service'>
-            <h3 className="card-title">Lorem, ipsum dolor.</h3>
-            <p className="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem soluta ipsum neque deserunt praesentium animi iste quas repellat doloribus.</p>
-        </div> 
+          <div className="service-navbar">
+            <h3 className="navbar-title">START UPS</h3>
+          </div>
+          <div className="service-content">
+            <h3 className="card-title"></h3>
+            <p className="card-content">
+              You have recently started and are seeking a logistics partner so you can stay focused on production, development, marketing, and growth.
+            </p>
+            <a href="#" className="learn-more">Learn More</a>
+          </div>
+        </div>
         <div className='service'>
-            <h3 className="card-title">Lorem, ipsum dolor.</h3>
-            <p className="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem soluta ipsum neque deserunt praesentium animi iste quas repellat doloribus.</p>
-        </div> 
+          <div className="service-navbar">
+            <h3 className="navbar-title">MSME</h3>
+          </div>
+          <div className="service-content">
+            <h3 className="card-title"></h3>
+            <p className="card-content">
+              You are a stable business and want to further expand and reduce your costs.
+            </p>
+            <a href="#" className="learn-more">Learn More</a>
+          </div>
+        </div>
         <div className='service'>
-            <h3 className="card-title">Lorem, ipsum dolor.</h3>
-            <p className="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem soluta ipsum neque deserunt praesentium animi iste quas repellat doloribus.</p>
-        </div>       
-  </div>
-  )
+          <div className="service-navbar">
+            <h3 className="navbar-title">ENTERPRISE</h3>
+          </div>
+          <div className="service-content">
+            <h3 className="card-title"></h3>
+            <p className="card-content">
+              Well-established company, you want a full-service logistics company that has the know-how and can deliver on your standard SOPs.
+            </p>
+            <a href="#" className="learn-more">Learn More</a>
+          </div>
+        </div>
+      </div>
+      <button className="btn" onClick={openForm}>CONTACT US</button>
+      {isFormOpen && <Form closeForm={closeForm} />}
+    </div>
+  );
 }
 
-export default Services
+export default Services;
