6baea707974bd83a4f7d94a8eef70b93ab6ed1f4
Yash Rawat <yashrawat1905@gmail.com>
2024-11-15 15:42:00 +0530

    locations page issue

--- src/Components/Locations/HeroSection.jsx
+++ src/Components/Locations/HeroSection.jsx
@@ -1,28 +1,36 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './HeroSection.css';
 import AOS from 'aos'; // Import AOS
 import 'aos/dist/aos.css'; // Import AOS CSS 
+import Form from '../Form/Form';
+
 
 const HeroSection = ({ location, image }) => {
+
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const openForm = () => setIsFormOpen(true);
+  const closeForm = () => setIsFormOpen(false);
 
   useEffect(() => {
     AOS.init();
   }, []);
 
-  return (
+  return ( 
     <section className="hero-section">
-      <div className="hero-content">
+      <div className="hero-content-location">
         <h1 className="hero-title">Warehouse in {location}</h1>
         <p className="hero-subtitle">
           From cost-effective warehouses to high-end fulfillment facilities, Nagarkot is the go-to provider for warehousing services in {location} for all your business needs.
         </p>
-        <button className="hero-button">GET STARTED</button>
+        <button className="hero-button" onClick={openForm}>GET STARTED</button>
       </div>
       <div className="hero-image" data-aos="fade-left" data-aos-duration="15000">
         <img src={image} alt={`Warehouse in ${location}`} />
       </div>
+      {/* Contact Form Modal */}
+      {isFormOpen && <Form closeForm={closeForm} />}
     </section>
   );
 };
